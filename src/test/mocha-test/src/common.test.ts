c4bd800af809c5c93f813827dce842f4aac2053f
liaozhenyu_brian <brianliu1229@outlook.com>
2021-12-10 14:43:59 +0800

    upgrade version (to v1.1)

--- src/test/mocha-test/src/common.test.ts
+++ src/test/mocha-test/src/common.test.ts
@@ -10,7 +10,7 @@ let dateTimeNoZonePattern = /^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]
 let dateTimeAndMsNoZonePattern = /^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}[.][0-9]*Z$/;
 let base64Pattern = /^[a-zA-Z0-9\/\+\=]+$/;
 
-let apiBaseUrl = 'http://127.0.0.1:8080/api/1.0';
+let apiBaseUrl = 'http://127.0.0.1:8080/api/1.1';
 
 let options = {
   // echoReq: 'simple',
